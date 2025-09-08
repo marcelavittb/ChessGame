ca7f62154c5b397c9fb16d26c15a22263df24fa6
Marcela becher <marcelabecher0@gmail.com>
2025-09-07 23:03:49 -0300

    Alterações no design e movimentações das peças

--- src/dragger.py
+++ src/dragger.py
@@ -12,22 +12,28 @@ class Dragger:
         self.initial_row = 0
         self.initial_col = 0
 
+    # blit method
+
     def update_blit(self, surface):
+        # texture
         self.piece.set_texture(size=128)
         texture = self.piece.texture
+        # img
         img = pygame.image.load(texture)
-
-        img_center = self.mouseX, self.mouseY
+        # rect
+        img_center = (self.mouseX, self.mouseY)
         self.piece.texture_rect = img.get_rect(center=img_center)
-
+        # blit
         surface.blit(img, self.piece.texture_rect)
 
+    # other methods
+
     def update_mouse(self, pos):
-        self.mouseX, self.mouseY = pos #(xcor, ycor)
+        self.mouseX, self.mouseY = pos # (xcor, ycor)
 
     def save_initial(self, pos):
         self.initial_row = pos[1] // SQSIZE
-        self.initial_col = pos[0] // SQSIZE  
+        self.initial_col = pos[0] // SQSIZE
 
     def drag_piece(self, piece):
         self.piece = piece
@@ -36,5 +42,3 @@ class Dragger:
     def undrag_piece(self):
         self.piece = None
         self.dragging = False
-       
-
